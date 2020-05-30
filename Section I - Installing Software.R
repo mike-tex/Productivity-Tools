ee768c57ae3e8575078ecae681e1fe33e8467bcb
Michael W. Jones <mike-tex@swbell.net>
2020-05-30 16:37:03 -0500

    first sync

---
+++ Section I - Installing Software.R
@@ -0,0 +1,143 @@
+## Section 1: Installing Software
+##   1.2 Introduction to RStudio
+##   Installing Packages
+
+# Code
+install.packages("dslabs")  # to install a single package
+install.packages(c("tidyverse", "dslabs")) # to install two packages at the same time
+installed.packages() # to see the list of all installed packages
+
+## Running Commands While Editing Scripts
+# Code
+library(tidyverse)
+
+## 1.3 Introduction to Git and GitHub
+##   Installing Git Introduction
+
+## Installing Git
+
+# Install on Windows
+# 1. Download Git bash from https://git-scm.com/
+# 2. When asked to choose the default editor for Git, 
+#    we recommend choosing nano if you do not already know VIM.
+# 3. The "git and optional Unix tools from Windows" option 
+#    will allow you to learn Unix from Rstudio, however, 
+#    it might interfere with the Windows command line.
+# 4. Finally, change the Rstudio preference so that you are 
+#    using Git bash as the terminal (only for Windows user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
